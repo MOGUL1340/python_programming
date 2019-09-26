c7b089048f5e29f1fad5f36396b60fd3043d1f09
Mogul1340 <antonioua1340@gmail.com>
2019-09-26 21:10:58 +0300

    removed add_contact added new test New_contact

--- contact.py
+++ contact.py
@@ -1,9 +1,9 @@
 class Contact:
-    def __init__(self, firstname, middlename, mobile_num,):
+    def __init__(self, firstname, lastname, mobile):
         self.firstname = firstname
-        self.middlename = middlename
-        self.mobile_num = mobile_num
-        # self.lastname = lastname
+        # self.middlename = middlename
+        self.mobile = mobile
+        self.lastname = lastname
         # self.nickname = nickname
         # self.byear = byear
         # self.address = address
---
+++ test_new_contact.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.firefox import webdriver
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+from contact import Contact
+import unittest
+
+class TestNewContact(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+
+    def test_new_contact(self):
+        wd = self.wd
+        self.open_homepage(wd)
+        self.login(wd)
+        self.new_contact(wd, Contact(firstname="Adi", lastname="Das", mobile="+30480001234"))
+        self.return_homepage(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_homepage(self, wd):
+        wd.find_element_by_link_text("home page").click()
+
+    def new_contact(self, wd, contact):
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_homepage(self, wd):
+        wd.get("http://localhost/addressbook/index.php")
+
+    def is_element_present(self, how, what):
+        try: self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+
+    def is_alert_present(self):
+        try: self.wd.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.wd.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()

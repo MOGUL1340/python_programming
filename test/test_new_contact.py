4646d305c2db42150b96d575aff883112555481e
Mogul1340 <antonioua1340@gmail.com>
2019-09-27 09:18:15 +0300

    added new class session

--- test/test_new_contact.py
+++ test/test_new_contact.py
@@ -12,10 +12,10 @@ def app(request):
 
 def test_new_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.new_contact(Contact(firstname="Adi", lastname="Das", mobile="+30480001234"))
     app.return_homepage()
-    app.logout()
+    app.session.logout()
 
 
 

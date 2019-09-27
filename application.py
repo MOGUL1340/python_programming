73acb0e7e7844387fd59beeca1e1b0d51b94cc92
Mogul1340 <antonioua1340@gmail.com>
2019-09-27 08:56:08 +0300

    refactor test_new_contact

--- application.py
+++ application.py
@@ -62,6 +62,25 @@ class Application:
         wd = self.wd
         wd.get("http://localhost/addressbook/")
 
+    def return_homepage(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def new_contact(self, contact):
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
     def is_element_present(self, how, what):
         try:
             self.wd.find_element(by=how, value=what)

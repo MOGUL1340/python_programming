421e4f51ce6172ef5691fb25fa373df6ae65982e
MOGUL1340 <antonioua1340@gmail.com>
2019-10-07 11:07:25 +0300

    fixed method

--- fixture/group_fixture.py
+++ fixture/group_fixture.py
@@ -30,8 +30,9 @@ class GroupHelper:
 
     def open_group_page(self):
         wd = self.app.wd
-        if not (wd.current_url.endwith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
             wd.find_element_by_link_text("groups").click()
+
 
     def delete_first_group(self):
         wd = self.app.wd

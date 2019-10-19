a406ed41fc541c3df9542e7ea1a44aee54930a8e
MOGUL1340 <antonioua1340@gmail.com>
2019-10-19 12:24:58 +0300

    added new assertions to grioup tests

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,10 +2,16 @@
 from model.group import Group
 
 def test_empty_group(app):
+    old_groups = app.group_fixture.get_group_list()
     app.group_fixture.create_group(Group(name="", header="", footer=""))
+    new_groups = app.group_fixture.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_group(app):
+    old_groups = app.group_fixture.get_group_list()
     app.group_fixture.create_group(Group(name="adsfger", header="ahdhgftr", footer="agdgfdrer"))
+    new_groups = app.group_fixture.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 
